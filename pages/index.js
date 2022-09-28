66f3a3555c152f0f3750bd9dc08bdff6d16983fd
Alejandro <alejandroaperez1994g@gmail.com>
2022-09-28 02:22:09 +0200

    fix: :fire: remove unused code

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ import Sidebar from '../components/Sidebar';
 
 import styles from '../styles/Home.module.css';
 
-export default function Home({ access_token, token_type }) {
+export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
